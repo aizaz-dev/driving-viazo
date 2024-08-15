bab23a410a651260105fc74ff5658317e3899bb3
Sateesh Madagoni <sateeshmadagoni@MacBook-Pro.local>
2024-08-15 11:47:24 -0400

    Fix somebugs

--- src/components/Drivers/Pricing/Pricing.js
+++ src/components/Drivers/Pricing/Pricing.js
@@ -97,7 +97,7 @@ const Pricing = ({ title, description, pricingPlans }) => {
                 className={`mt-5 py-3 px-4 inline-flex justify-center items-center gap-x-2 text-sm font-medium rounded-lg ${
                   plan.popular
                     ? "border border-transparent bg-blue-600 text-white hover:bg-blue-700 focus:outline-none focus:bg-blue-700"
-                    : "border border-gray-200 bg-white text-gray-800 shadow-sm hover:bg-gray-50 focus:outline-none focus:bg-gray-50 dark:bg-transparent dark:border-neutral-700 dark:text-neutral-300 dark:hover:bg-neutral-800 dark:focus:bg-neutral-800"
+                    : "border border-gray-200 bg-main-600 text-white shadow-sm hover:bg-main-500 focus:outline-none focus:bg-main-500 dark:bg-transparent dark:border-neutral-700 dark:text-neutral-300 dark:hover:bg-neutral-800 dark:focus:bg-neutral-800"
                 }`}
                 href="#"
               >
